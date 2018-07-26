c6cb662340d0703e465f484775ccaec83b1d10ed
Padma <bommapaddu@gmail.com>
2018-07-26 00:31:00 -0700

    added app.properties file to configure the webdriver path

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # matrix
+before running tests 
+1. download the Chrome driver from url  https://chromedriver.storage.googleapis.com/index.html?path=2.38/ and unzip in a folder 
+2. and update the src\main\resources\app.properties file with the correct path for chormedriver.exe path (from above step)
+
+run below command to execute test cases
+
+mvn test
--- src/test/java/com/padma/automation/weigthtwatchers/BaseTestCase.java
+++ src/test/java/com/padma/automation/weigthtwatchers/BaseTestCase.java
@@ -1,5 +1,9 @@
 package com.padma.automation.weigthtwatchers;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -7,11 +11,19 @@ import org.testng.annotations.BeforeTest;
 
 public class BaseTestCase {
 	WebDriver driver;
+	Properties config = new Properties();
 
 	@BeforeTest
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Padma\\ChromeDriver\\chromedriver.exe");
+		loadConfig();
+
+		String defaultDriverPath = "C:\\Padma\\ChromeDriver\\chromedriver.exe";
+
+		if (config.getProperty("webdriver.chrome.driver") != null) {
+			defaultDriverPath = config.getProperty("webdriver.chrome.driver");
+		}
+		System.setProperty("webdriver.chrome.driver", defaultDriverPath);
+
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	}
@@ -19,6 +31,31 @@ public class BaseTestCase {
 	@AfterTest
 	public void tearDown() {
 		driver.quit();
+	}
+
+	private void loadConfig() {
+
+		InputStream input = null;
+
+		try {
+			String filename = "app.properties";
+			// input = new FileInputStream("app.properties");
+			input = BaseTestCase.class.getClassLoader().getResourceAsStream(
+					filename);
+			// load a properties file
+			config.load(input);
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 }
